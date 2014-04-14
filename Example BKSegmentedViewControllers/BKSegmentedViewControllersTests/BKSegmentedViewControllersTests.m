462169e73d1fc17c94ebc2af7d1b760b4175b931
bhavya-kothari <mr.bhavya.kothari@gmail.com>
2014-04-14 20:13:59 +0530

    Initial Commit

---
+++ Example BKSegmentedViewControllers/BKSegmentedViewControllersTests/BKSegmentedViewControllersTests.m
@@ -0,0 +1,34 @@
+//
+//  BKSegmentedViewControllersTests.m
+//  BKSegmentedViewControllersTests
+//
+//  Created by Riddhi on 12/04/14.
+//  Copyright (c) 2014 bhavya. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BKSegmentedViewControllersTests : XCTestCase
+
+@end
+
+@implementation BKSegmentedViewControllersTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
